35f9bdcf126ede9ffad917f80b937d7759f777a7
Im-Siyoun <rlatldbs3321@sju.ac.kr>
2025-03-04 12:02:38 +0900

    feat: 커스텀 세션 분리리

--- auth/session_login.py
+++ auth/session_login.py
@@ -1,46 +1,37 @@
-import json
-import bs4 as bs
 import requests
-import urllib3
 from requests import Session
 
-def login(id, password, max_retries=3, timeout=1):
-    s = Session()
+class CustomSession:
+    def __init__(self, session=None, headers=None, cookies=None):
+        self.headers = headers or {}
+        self.cookies = cookies or {}
+        self.session = session or Session()
 
-    headers = {
-        "Host": "portal.sejong.ac.kr",
-        "Referer": "https://portal.sejong.ac.kr"
-    }
+    def login(self, id, password, max_retries=3, timeout=1):
+        data = {
+            "mainLogin": "N",
+            "rtUrl": "library.sejong.ac.kr",
+            "id": id,
+            "password": password,
+        }
 
-    cookies = {
-        "chknos": "false",
-    }
+        url = "https://portal.sejong.ac.kr/jsp/login/login_action.jsp"
 
-    data = {
-        "mainLogin": "N",
-        "rtUrl": "library.sejong.ac.kr",
-        "id": id,
-        "password": password,
-    }
-
-    url = "https://portal.sejong.ac.kr/jsp/login/login_action.jsp"
-
-    for i in range(max_retries):
-        try:
-            r1 = s.post(
-                url,
-                headers=headers,
-                cookies=cookies,
-                data=data,
-                timeout=timeout,
-            )
-            if "ssotoken" in r1.cookies:
-                return r1.cookies
-            break
-        except requests.exceptions.Timeout:
-            continue
-        except requests.exceptions.RequestException as e:
-            print(f"로그인 오류 발생: {e} 재시도...{i}")
-
-    if not "ssotoken" in r1.cookies:
-        return 401, "로그인 실패"
+        for i in range(max_retries):
+            try:
+                r1 = self.session.post(
+                    url,
+                    headers=self.headers,
+                    cookies=self.cookies,
+                    data=data,
+                    timeout=timeout,
+                )
+                if "ssotoken" in r1.cookies:
+                    self.cookies["ssotoken"] = r1.cookies["ssotoken"]
+                    self.headers["Cookie"] = f"chknos=false;"
+                return
+            except requests.exceptions.Timeout:
+                continue
+            except requests.exceptions.RequestException as e:
+                print(f"로그인 오류 발생: {e} 재시도...{i+1}")
+        raise RuntimeError("로그인 실패")
